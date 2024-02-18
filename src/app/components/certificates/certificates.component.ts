201ad56290c938352fd145b55378f97d9e397d03
Mohammad-abdo <sdm800884@gmail.com>
2024-02-18 13:51:13 +0200

    update

--- src/app/components/certificates/certificates.component.ts
+++ src/app/components/certificates/certificates.component.ts
@@ -10,15 +10,12 @@ export class CertificatesComponent {
 certifics:any=
 [
 
-  // {img:'../../../assets/images/FullStack.svg',textImg:'ITI Full Stack',link:''},
-  // {img:'../../../assets/images/NodeJs.svg',textImg:'Node Js',link:''},
-  // {img:'../../../assets/images/IntroToJavaScript.svg',textImg:'Intro To JS',link:''},
-  // {img:'../../../assets/images/Agile.svg',textImg:'Agile',link:''},
+  
   {img:'../../../assets/images/certificat/mong-trans.png',textImg:'MongoDB Transactions',link:''},
   {img:'../../../assets/images/certificat/mongo-intro.png',textImg:'MongoDB ',link:''},
   {img:'../../../assets/images/certificat/kamsat.png',textImg:'Freelancing Kamsat',link:''},
   {img:'../../../assets/images/certificat/fwd.png',textImg:'Professional Front-End FWD',link:''},
-  // {img:'../../../assets/images/UIX.svg',textImg:'UX Design Fundamentals',link:''},
+  {img:'../../../assets/images/certificat/mostacl.png',textImg:'Freelancing mostaql',link:''},
   {img:'../../../assets/images/certificat/html.png',textImg:'HTML & CSS',link:''},
   {img:'../../../assets/images/certificat/database.png',textImg:'Database Fundamentals',link:''},
 
